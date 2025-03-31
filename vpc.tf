05f244a07bcd09cb13e7fb266fd6fdf2996b3be8
SurajZadane13 <SurajZadane13@gmail.com>
2025-04-01 04:01:37 +0530

    create subnet

--- vpc.tf
+++ vpc.tf
@@ -7,3 +7,35 @@ resource "aws_vpc" "mytf-vpc" {
     Name = "mytf-vpc"
   }
 }
+
+#web subnet
+resource "aws_subnet" "web-subnet" {
+  vpc_id     = aws_vpc.mytf-vpc.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch  = "true"
+
+  tags = {
+    Name = "web-subnet"
+  }
+}
+
+#api subnet
+resource "aws_subnet" "api-subnet" {
+  vpc_id     = aws_vpc.mytf-vpc.id
+  cidr_block = "10.0.2.0/24"
+  map_public_ip_on_launch  = "true"
+
+  tags = {
+    Name = "api-subnet"
+  }
+}
+
+#db subnet
+resource "aws_subnet" "db-subnet" {
+  vpc_id     = aws_vpc.mytf-vpc.id
+  cidr_block = "10.0.3.0/24"
+
+  tags = {
+    Name = "db-subnet"
+  }
+}
